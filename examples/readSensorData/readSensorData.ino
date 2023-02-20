85abeea6c97054cc39bbda86ac85b4ecb92eb373
autoHammer <60541285+autoHammer@users.noreply.github.com>
2023-02-20 23:01:29 +0100

    Make example code

--- examples/readSensorData/readSensorData.ino
+++ examples/readSensorData/readSensorData.ino
@@ -1,39 +1,48 @@
-//example code for EMS22 library - Absolute Rotary Encoder
+// Example code for EMS22 library - Absolute Rotary Encoder
 // Written by William Hammer, NTNU 2023.
 
-// get the library at https://github.com/autoHammer/EMS22
-#include "AbsEncoder.h"
+// This example supports daisy chained encoders.
+// Daisy chain means connecting output from one encoder into input of another encoder.
 
+// Get the library at https://github.com/autoHammer/EMS22
+#include "EMS22.h"
 
-const int CLK = 11; //lilla
-const int DO  = 10; //blå digital output from encoder
-const int CS  = 9;  //Grønn (gul)
+// Check connection in the datasheet at https://www.elfadistrelec.no/Web/Downloads/_t/ds/EMS22A50-B28-LS6_eng_tds.pdf
+const int CLK = 11;   // encoder pin 2
+const int DO  = 10;   // encoder pin 4
+const int CS  = 9;    // encoder pin 6
 
+// Declare object
 AbsEncoder Coder(CLK, DO, CS);
-
-//bool data[2][16];
 
 void setup() {
   Serial.begin(9600);
   while (!Serial); // wait for serial to establish
 
+  // Initialize object
   Coder.begin();
 }
 
 void loop() {
-  Coder.scan();
+  // Read the value of all encoders. Data is stored in the object Coder.
+  // Must be ran every time data needs to be updated.
+  Coder.read();
 
+  // Fetch data. Input is the encoder index.
+  // The encoder connected closest to the Arduino has index 0.
+  // AnalogData is a number between 0 and 1024 (one loop).
+  int encoder0Data = Coder.getAnalogData(0)
+  //Serial.println(encoder0Data);
+
+  // plotAngles will format and print all angle data.
+  // Use Serial plotter under "Tools" to see the result.
+  Coder.plotAngles();
+
+  // Store all analog data into a list.
+  // First index is first connected encoder.
   int dataList[2];
   Coder.copyAllAnalog(dataList);
-
-  Coder.plotAngles();
-  delay(1);
-
-  //Coder.copyRawData(data);
-  //Coder.printRaw(0);
-
-  //Serial.println(Coder.getAnalogData(0));
-
   //Serial.println(dataList[0]);
-  //Serial.println(dataList[1]);
+
+  delay(1);
 }
